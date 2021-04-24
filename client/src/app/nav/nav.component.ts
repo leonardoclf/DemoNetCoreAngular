1774aa7e6795f20ff472e42505b2b046310b0fe3
leonardoclf <leonardoclf@gmail.com>
2021-04-24 16:45:51 -0300

    refactor login logic

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../_models/user';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,18 +13,15 @@ export class NavComponent implements OnInit {
   // obj default = null
   model: any = {}
   // boolean default = false 
-  loggedIn: boolean;
   
-  constructor(private accountService: AccountService) { }
+  
+  constructor(public accountService: AccountService) { }
 
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
+  ngOnInit(): void { }
 
   login() {
     this.accountService.login(this.model).subscribe(response => {
       console.log(response);
-      this.loggedIn = true;
     }, error => {
       console.log(error);
     })
@@ -30,17 +29,7 @@ export class NavComponent implements OnInit {
 
   logout() {
     this.accountService.logout();
-    this.loggedIn = false;
   }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(user => {
-      this.loggedIn = !!user;
-    }, error => {
-      console.log(error);
-    })
-  }
-
 }
 
 /*
